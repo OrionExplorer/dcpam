db0da23a7dd1049ef7461f33f40588ae1c52996a
Marcin Kelar <m.kelar@intalio.pl>
2020-06-16 18:56:13 +0200

    Fixes to log.c

--- src/utils/log.c
+++ src/utils/log.c
@@ -14,32 +14,21 @@ char                        LOG_filename[MAX_PATH_LENGTH];
 static pthread_mutex_t      printf_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static void LOG_prepare( void ) {
-    char *tmp_path = calloc( MAX_PATH_LENGTH_CHAR + 1, sizeof( char ) );
 
-    strncpy( tmp_path, app_path, MAX_PATH_LENGTH );
-    strncat( tmp_path, LOGS_PATH, MAX_PATH_LENGTH );
-
-    strncpy( LOG_filename, tmp_path, MAX_PATH_LENGTH );
-    strncat( LOG_filename, "log.txt", MAX_PATH_LENGTH - strlen( LOG_filename ) );
-
+    snprintf( LOG_filename, MAX_PATH_LENGTH, "%s%slog.txt", app_path, LOGS_PATH );
     LOG_print( "\n[%s] Session start.\n", TIME_get_gmt() );
 
-    free( tmp_path );
-    tmp_path = NULL;
 }
 
 
 static void LOG_validate_paths( void ) {
-    char *tmp_path = malloc( MAX_PATH_LENGTH_CHAR + 1 );
-    int res = -1;
+    char *log_path = malloc( MAX_PATH_LENGTH_CHAR + 1 );
 
-    if( tmp_path ) {
-        strncpy( tmp_path, app_path, MAX_PATH_LENGTH );
-
-        strncat( tmp_path, LOGS_PATH, MAX_PATH_LENGTH );
-        if( directory_exists( tmp_path ) == 0 ) {
-            if( mkdir( tmp_path, 0777 ) != 0 ) {
-                LOG_print( "Error ( %d ) creating path!\n", res );
+    if( log_path ) {
+        snprintf( log_path, MAX_PATH_LENGTH, "%s%s", app_path, LOGS_PATH );
+        if( directory_exists( log_path ) == 0 ) {
+            if( mkdir( log_path, 0777 ) != 0 ) {
+                LOG_print( "Error creating path!\n" );
             }
             if( chdir( app_path ) != 0 ) {
                 LOG_print( "Error: chdir().\n" );
@@ -49,8 +38,8 @@ static void LOG_validate_paths( void ) {
 
         LOG_prepare();
 
-        free( tmp_path );
-        tmp_path = NULL;
+        free( log_path );
+        log_path = NULL;
     } else {
         LOG_print( "LOG_validate_paths error: unable to allocate memory.");
     }
@@ -58,7 +47,8 @@ static void LOG_validate_paths( void ) {
 }
 
 void LOG_init( void ) {
-    strncpy( app_path, get_app_path(), MAX_PATH_LENGTH );
+    snprintf( app_path, MAX_PATH_LENGTH, "%s", get_app_path() );
+    /*strncpy( app_path, get_app_path(), MAX_PATH_LENGTH );*/
 
     ( void )LOG_validate_paths();
 
@@ -72,8 +62,6 @@ void LOG_print( char *fmt, ... ) {
     char *output_text;
     FILE *f_LOG;
     va_list args;
-    int buf_len = 0;
-    int len = 0;
 
     pthread_mutex_lock( &printf_mutex );
     output_text = ( char* )calloc( LARGE_BUFF_SIZE + 1, sizeof( char ) );
